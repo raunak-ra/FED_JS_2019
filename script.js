858d4ef8d41ba43777307139650843502c3fa323
tavisca-rsingh <rsingh@tavisca.in>
2019-08-22 21:29:35 +0530

    Updated auto

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showContent(element) {
   for (var i = 0; i < items.length; i++) {
     items[i].style.display = "none";
   }
-    document.getElementById(element.id +"-popup").style.display="block";
+  document.getElementById(element.id + "-popup").style.display = "block";
 }
 
 
@@ -11,56 +11,50 @@ function showContent(element) {
 
 
 counter = 0;
-    var items= ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India"];
+var items = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua & Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia & Herzegovina", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India","last"];
 
 
 
 function addItems() {
-  if (document.getElementById('itemName').value != "") {
-    var itemName = document.getElementById('itemName').value;
-    counter += 1;
-    document.getElementById('items').innerHTML += "<br>" + counter + ". " + itemName;
-    document.getElementById('itemName').value = "";
-    items.push(itemName);
-  }
+  let value = document.getElementById('myInput').value;
+  let li = document.createElement('li');
+  li.innerText = value;
+  document.getElementById('task-list').appendChild(li);
+  document.getElementById('myInput').value = "";
+
 }
 
 
 function showTab(ele) {
   var elements = document.getElementsByClassName('tab');
   for (var i = 0; i < elements.length; i++)
-      elements[i].style.display = "none";
+    elements[i].style.display = "none";
   document.getElementById(ele.name).style.display = "block";
 }
-function setItem(ele,flag)
-{
+
+function setItem(ele, flag) {
   document.getElementById('myInput').value = ele.innerHTML;
-  if(flag)
+  if (flag)
     document.getElementById('suggestions-list').style.display = "none";
 }
-function showSuggestions(element)
-{
+
+function showSuggestions(element) {
   document.getElementById('suggestions-list').style.display = "none";
-    document.getElementById('list').innerHTML="";
-    txt = element.value;
-    for(var i=0;i<items.length;i++)
-    {
-      if(items[i].startsWith(txt))
-      {
-        var textNode = document.createTextNode(items[i]);
-        var ele = document.createElement("li");
-        ele.append(textNode);
-        ele.onmouseover=function() {
-          setItem(this,false);
-        };
-        ele.onclick=function() {
-          setItem(this,true);
-        };
-        //ele.addEventListener("click",function() {setItem(items[i])})
-        document.getElementById("list").append(ele);
+  document.getElementById('list').innerHTML = "";
+  txt = (element.value).toUpperCase();
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].startsWith(txt)) {
+      var textNode = document.createTextNode(items[i]);
+      var ele = document.createElement("li");
+      ele.append(textNode);
+      ele.onmouseover = function() {
+        setItem(this, false);
+      };
+      ele.onclick = function() {
+        setItem(this, true);
+      };
+      document.getElementById("list").append(ele);
     }
-    }
-    document.getElementById('suggestions-list').style.display = "block";
-
-
+  }
+  document.getElementById('suggestions-list').style.display = "block";
 }
